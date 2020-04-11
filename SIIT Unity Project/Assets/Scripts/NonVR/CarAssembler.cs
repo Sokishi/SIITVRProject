4ac1561886c2d6bf7bf9dc832f427b709452224d
mkdesilva <mihindu.desilva@gmail.com>
2020-04-11 22:51:11 +0700

    Added basic car assembly when pressing space bar

---
+++ SIIT Unity Project/Assets/Scripts/NonVR/CarAssembler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NonVR
+{
+    public class CarAssembler : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> parts;
+        private int currentPartIndex;
+
+        private void Awake()
+        {
+            currentPartIndex = 0;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (currentPartIndex < parts.Count)
+                {
+                    parts[currentPartIndex].SetActive(true);
+                    currentPartIndex++;
+
+                }
+            }
+        }
+    }
+}
