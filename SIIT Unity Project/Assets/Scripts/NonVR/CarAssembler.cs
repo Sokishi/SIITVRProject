ecd7f5a51c13e262c73ee76d32bb0c7fa355cc21
MK de Silva <dev.mkdesilva@gmail.com>
2020-04-17 18:46:18 +0700

    Added requirement steps to CarAssembler

--- SIIT Unity Project/Assets/Scripts/NonVR/CarAssembler.cs
+++ SIIT Unity Project/Assets/Scripts/NonVR/CarAssembler.cs
@@ -1,4 +1,6 @@
-﻿using echo17.Signaler.Core;
+﻿using System.Collections.Generic;
+using System.Linq;
+using echo17.Signaler.Core;
 using Signals;
 using UnityEngine;
 
@@ -6,22 +8,56 @@ namespace NonVR
 {
     public class CarAssembler : MonoBehaviour, IBroadcaster, ISubscriber
     {
+        [SerializeField] private List<CarAssemblyPart.PartType> requirements = new List<CarAssemblyPart.PartType>();
+        private int currentRequirementIndex;
         private CarAssemblyPart[] parts;
         private bool isComplete => IsAssemblyComplete();
-
         // Wow don't do this
         private GameObject copy;
-        private readonly AssemblySignals.AssemblyCompleteSignal assemblyCompleteSignal = new AssemblySignals.AssemblyCompleteSignal();
+        private readonly AssemblySignals.AssemblyCompleteSignal assemblyCompleteSignal =
+            new AssemblySignals.AssemblyCompleteSignal();
         private MessageSubscription<AssemblySignals.AssembledPartSignal> assembledPartSignalSubscription;
-
+        private CarAssemblyPart.PartType currentPartRequirement => requirements[currentRequirementIndex];
+        
         private void Awake()
         {
             parts = GetComponentsInChildren<CarAssemblyPart>();
+            ValidateRequirements();
+
+            if (requirements.Count > 0)
+                currentRequirementIndex = 0;
+
+            foreach (var part in parts)
+            {
+                part.SetAssembler(this);
+            }
+        }
+
+        private void ValidateRequirements()
+        {
+            var partTypesInAssembler = new List<CarAssemblyPart.PartType>();
+            foreach (var part in parts)
+            {
+                if (!partTypesInAssembler.Contains(part.GetPartType))
+                {
+                    partTypesInAssembler.Add(part.GetPartType);
+                }
+            }
+
+            foreach (var requirement in requirements)
+            {
+                if (!partTypesInAssembler.Contains(requirement))
+                {
+                    Debug.LogError("Assembler does not contain part type: " + requirement +
+                                   ". Check the setup for CarAssembler.");
+                }
+            }
         }
 
         private void OnEnable()
         {
-            assembledPartSignalSubscription = Signaler.Instance.Subscribe<AssemblySignals.AssembledPartSignal>(this, AssembledPart);
+            assembledPartSignalSubscription =
+                Signaler.Instance.Subscribe<AssemblySignals.AssembledPartSignal>(this, AssembledPart);
         }
 
         private void OnDisable()
@@ -44,12 +80,31 @@ namespace NonVR
             if (!enabled)
                 return false;
 
+            // Check if all parts of currentRequirement type have been assembled
+            // if not, do nothing
+            // if they have, move to next requirement
+            ProgressRequirementIfCompleted();
+
             if (isComplete)
                 AssemblyComplete();
 
             return true;
         }
-        
+
+        private void ProgressRequirementIfCompleted()
+        {
+            var currentRequirementParts = parts.Where(part => part.GetPartType == currentPartRequirement);
+            var hasCompletedRequirement = currentRequirementParts.All(part => part.isAssembled);
+
+            if (hasCompletedRequirement)
+            {
+                if (currentRequirementIndex < requirements.Count)
+                {
+                    currentRequirementIndex++;
+                }
+            }
+        }
+
         private void AssemblyComplete()
         {
             Signaler.Instance.Broadcast(this, assemblyCompleteSignal);
@@ -60,10 +115,15 @@ namespace NonVR
         {
             foreach (var part in parts)
             {
-                if (!part.isComplete) return false;
+                if (!part.isAssembled) return false;
             }
 
             return true;
+        }
+
+        public bool CanAssemblePart(CarAssemblyPart.PartType partType)
+        {
+            return currentPartRequirement == partType;
         }
     }
 }
--- SIIT Unity Project/Assets/Scripts/NonVR/CarAssemblyPart.cs
+++ SIIT Unity Project/Assets/Scripts/NonVR/CarAssemblyPart.cs
@@ -6,7 +6,7 @@ namespace NonVR
 {
     public class CarAssemblyPart : MonoBehaviour, IBroadcaster
     {
-        private enum PartType
+        public enum PartType
         {
             Roof,
             Hood,
@@ -14,15 +14,20 @@ namespace NonVR
             Wheel,
             FrameSide
         }
-        
         [SerializeField] private PartType partType;
-        [SerializeField] private bool isStaticPart = false; // non-movable part attached to the car for detection purposes
-      
-        [HideInInspector] public bool isComplete = false;
+        // non-movable part attached to the car for detection purposes
+        [SerializeField] private bool isStaticPart = false;
+        public PartType GetPartType => partType;
+
+        [HideInInspector] public bool isAssembled = false;
 
         private GameObject part;
-        private AssemblySignals.AssembledPartSignal assembledPartSignal = new AssemblySignals.AssembledPartSignal();
-        
+
+        private readonly AssemblySignals.AssembledPartSignal assembledPartSignal =
+            new AssemblySignals.AssembledPartSignal();
+
+        private CarAssembler assembler;
+
         private void Awake()
         {
             // TODO: Don't do this. This is a terrible way
@@ -32,24 +37,28 @@ namespace NonVR
         private void OnTriggerEnter(Collider other)
         {
             if (!isStaticPart) return; // Don't check for moving parts
-            
+
             // TODO: Check if part matches this.carType;
             if (other.isTrigger) return; // Another semi-redundant check for moving part
 
             // TODO: Don't do this, should be GetComponent only
             var otherPart = other.GetComponentInParent<CarAssemblyPart>();
-            if (otherPart == null)
-            {
-                print("Could not find CarAssemblyPart");
-                return;
-            }
-            
+            if (otherPart == null) return;
             if (otherPart.partType != partType) return;
-            
+
+            if (!assembler.CanAssemblePart(partType))
+                return;
+
             // TODO: LeanTween animation to proper part position + rotation
-            print(otherPart.name + " entered area of: " + part.name);
-            
-            // Disable triggers
+            DisableColliders();
+            Destroy(otherPart.gameObject);
+            part.gameObject.SetActive(true);
+            isAssembled = true;
+            Signaler.Instance.Broadcast(this, assembledPartSignal);
+        }
+
+        private void DisableColliders()
+        {
             var colliders = GetComponentsInChildren<Collider>();
             foreach (var col in colliders)
             {
@@ -58,11 +67,11 @@ namespace NonVR
                     col.enabled = false;
                 }
             }
-            
-            Destroy(otherPart.gameObject);
-            part.gameObject.SetActive(true);
-            isComplete = true;
-            Signaler.Instance.Broadcast(this, assembledPartSignal);
+        }
+
+        public void SetAssembler(CarAssembler carAssembler)
+        {
+            assembler = carAssembler;
         }
     }
 }
