e3ed0d2e64af54932902bee8ea92825f74aba76d
mkdesilva <dev.mkdesilva@gmail.com>
2020-04-23 21:35:27 +0700

    Added Grade Ui and signals for calculating grade when assembly is complete

---
+++ SIIT Unity Project/Assets/Scripts/Ui/UiUpdateGrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using echo17.Signaler.Core;
+using Signals;
+using TMPro;
+using UnityEngine;
+
+namespace Ui
+{
+   public class UiUpdateGrade : MonoBehaviour, ISubscriber
+   {
+      [SerializeField] private TMP_Text tmpText;
+
+      private void Awake()
+      {
+         Signaler.Instance.Subscribe<AssemblySignals.AchievedGradeSignal>(this, UpdateGradeText);
+      }
+
+      private bool UpdateGradeText(AssemblySignals.AchievedGradeSignal signal)
+      {
+         tmpText.text = "Achieved Grade: " + signal.Grade;
+         return true;
+      }
+   }
+}
